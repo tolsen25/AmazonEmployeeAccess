f37f1f3c91e03c0167d3b6d316805662c87fc336
Thomas Olsen <t.olsen18@outlook.com>
2023-10-13 13:53:19 -0600

    becker ran some files

--- penLogreg.R
+++ penLogreg.R
@@ -14,7 +14,7 @@ logRegModel = logistic_reg(mixture = tune(), penalty = tune()) %>%
 
 my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
-  step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
+  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
 
@@ -43,7 +43,7 @@ sub = testData %>% mutate(
 ) %>% select(Id, Action)
 
 
-vroom_write(sub, "penLogit.csv", delim = ",")
+vroom_write(sub, "penLogit_2.csv", delim = ",")
 
 
 
