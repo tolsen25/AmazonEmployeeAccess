50f294e1207319c1d8166e3b9ca8efccec290a26
Thomas Olsen <t.olsen18@outlook.com>
2023-10-17 12:50:08 -0600

    updated file names

--- randForest.R
+++ randForest.R
@@ -22,7 +22,7 @@ my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
 forestReg_workflow = workflow()  %>%
   add_recipe(my_recipe) %>% add_model(treeModel)
 
-tuning_grid = grid_regular(mtry(range = c(1,7)), min_n(), levels = 5) # idk what this does
+tuning_grid = grid_regular(mtry(range = c(1,7)), min_n(), levels = 5)# idk what this does
 
 folds = vfold_cv(trainData, v = 5, repeats = 1)
 
@@ -31,7 +31,7 @@ CV_results = forestReg_workflow %>% tune_grid(resamples = folds, grid = tuning_g
 
 bestTune = CV_results %>% select_best("roc_auc")
 
-final_wf = logReg_workflow %>% finalize_workflow(bestTune) %>% fit(trainData)
+final_wf = forestReg_workflow %>% finalize_workflow(bestTune) %>% fit(trainData)
 
 
 amazon_preds = predict(final_wf, new_data = testData, type = "prob")
@@ -44,5 +44,5 @@ sub = testData %>% mutate(
 ) %>% select(Id, Action)
 
 
-vroom_write(sub, "penLogit_2.csv", delim = ",")
+vroom_write(sub, "forestLogit_2.csv", delim = ",")
 
