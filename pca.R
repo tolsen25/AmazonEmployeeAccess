5c2f59d60a142b56d95a30ba3b21341dda23ce72
Thomas Olsen <t.olsen18@outlook.com>
2023-10-25 13:27:03 -0600

    dim reduction stuff and svm

---
+++ pca.R
@@ -0,0 +1,48 @@
+library(tidymodels)
+library(tidyverse)
+library(embed)
+library(vroom)
+library(discrim)
+
+
+trainData = vroom("train.csv") %>% mutate(ACTION = as.factor(ACTION))
+testData = vroom("test.csv")
+
+
+bayesRegModel = naive_Bayes(Laplace = tune(), smoothness= tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+
+my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
+  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .9012765)
+
+bayesReg_workflow = workflow()  %>%
+  add_recipe(my_recipe) %>% add_model(bayesRegModel)
+
+tuning_grid = grid_regular(Laplace(), smoothness(), levels = 5)
+
+folds = vfold_cv(trainData, v = 5, repeats = 1)
+
+CV_results = bayesReg_workflow %>% tune_grid(resamples = folds, grid = tuning_grid,
+                                             metrics = metric_set(roc_auc, precision))
+
+bestTune = CV_results %>% select_best("roc_auc")
+
+final_wf = bayesReg_workflow %>% finalize_workflow(bestTune) %>% fit(trainData)
+
+
+amazon_preds = predict(final_wf, new_data = testData, type = "prob")
+
+sub = testData %>% mutate(
+  Action = amazon_preds$.pred_1,
+  Id = id
+  
+) %>% select(Id, Action)
+
+
+vroom_write(sub, "pca_bayesLogit_2.csv", delim = ",")
--- pcaKNN.R
+++ pcaKNN.R
@@ -17,7 +17,8 @@ my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
   step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_normalize(all_numeric_predictors())
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .87654321)
 
 knn_workflow = workflow()  %>%
   add_recipe(my_recipe) %>% add_model(knnModel)
@@ -27,7 +28,7 @@ tuning_grid = grid_regular(neighbors(), levels = 5)
 folds = vfold_cv(trainData, v = 5, repeats = 1)
 
 CV_results = knn_workflow %>% tune_grid(resamples = folds, grid = tuning_grid,
-                                           metrics = metric_set(roc_auc, precision))
+                                        metrics = metric_set(roc_auc, precision))
 
 bestTune = CV_results %>% select_best("roc_auc")
 
@@ -44,4 +45,4 @@ sub = testData %>% mutate(
 ) %>% select(Id, Action)
 
 
-vroom_write(sub, "kknn2.csv", delim = ",")
+vroom_write(sub, "pca_kknn2.csv", delim = ",")
