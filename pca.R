6333d21c501f7efd93d8972c0cc8e6395790b363
Thomas Olsen <t.olsen18@outlook.com>
2023-10-30 12:31:33 -0600

    commenting out recipe

--- pca.R
+++ pca.R
@@ -14,12 +14,12 @@ bayesRegModel = naive_Bayes(Laplace = tune(), smoothness= tune()) %>%
   set_engine("naivebayes")
 
 
-my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
-  #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), threshold = .9012765)
+# my_recipe <- recipe(ACTION ~ ., data=trainData) %>%
+#   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors5
+#   #step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value6
+#   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+#   step_normalize(all_predictors()) %>%
+#   step_pca(all_predictors(), threshold = .9012765)
 
 bayesReg_workflow = workflow()  %>%
   add_recipe(my_recipe) %>% add_model(bayesRegModel)
